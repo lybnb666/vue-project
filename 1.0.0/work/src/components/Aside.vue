3cfc2a4c2a40ce90c9f0e76d4f973f9700c4227a
Honey <131582218+lybnb666@users.noreply.github.com>
2024-10-16 10:26:16 +0800

    Add files via upload

---
+++ 1.0.0/work/src/components/Aside.vue
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+<template>
+  <el-menu :default-openeds="['1', '3']" style="min-height: 100%;" background-color="rgb(48,65,86)"
+           text-color="#fff">
+    <div style="height:60px;line-height: 60px;text-align: center;">
+      <img src="../assets/logo.png" alt="" style="width: 20px; position: relative; top:3px; margin-right:5px ;">
+      <b style="color:white">后台管理系统</b>
+    </div>
+    <el-submenu index="1">
+      <template slot="title"><i class="el-icon-message"></i>主页</template>
+    </el-submenu>
+    <el-submenu index="2">
+      <template slot="title"><i class="el-icon-menu"></i>系统管理</template>
+      <el-menu-item-group>
+        <el-menu-item index="2-2">用户管理</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+  </el-menu>
+</template>
+
+<style>
+
+</style>
---
+++ 1.0.0/work/src/views/Login.vue
@@ -0,0 +1,34 @@
+<script>
+
+export default {
+  name: "Login",
+  data(){
+    return{
+      user:{}
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <div style="margin: 200px auto;background-color: #fff;width: 350px;height: 300px;padding: 20px;order-radius: 10px ">
+      <div style="margin: 20px 0;text-align: center;font-size: 24px"><b>登 录</b></div>
+      <el-input size="medium" style="margin: 10px 0" prefix-icon="el-icon-user" v-model = "user.username"></el-input>
+      <el-input size="medium" style="margin: 10px 0" prefix-icon="el-icon-lock" show-possword v-model="user.password"></el-input>
+      <div style="margin: 10px 0;text-align: right">
+        <el-button type="primary" size="small" autocomplete="off">登录</el-button>
+        <el-button type="warning" size="small" autocomplete="off">注册</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.wrapper{
+  height: 100vh;
+  background-image:linear-gradient(to bottom right,#FC466B, #3F5EFB);
+  overflow: hidden;
+}
+</style>
